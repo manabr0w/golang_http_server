7ca9f1214d6906615a4b908af3d6e9c515a70d42
maksim <maksimfedorenko71@gmail.com>
2025-02-28 21:47:33 +0200

    implemented function which shows time

--- main.go
+++ main.go
@@ -9,3 +9,9 @@ import (
 type TimeResponse struct {
 	Time string json:"time"
 }
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
